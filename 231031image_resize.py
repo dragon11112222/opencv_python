dba6c6ae4f15cbae73a46a56fecdb7df02ab4bce
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-31 14:30:17 +0900

    크기 조정 2
    원본에서 변경할 비율을 x, y 축 각각 입력.

--- 231031image_resize.py
+++ 231031image_resize.py
@@ -10,10 +10,12 @@ if img_BGR is None:
     sys.exit()
 
 imgResize = cv2.resize(img_BGR, (1280, 720))
+imgResize2 = cv2.resize(img_BGR, dsize = (0, 0), fx = 0.7, fy = 0.4)
 
 print(img_BGR.shape)
 
 cv2.imshow('lenna', img_BGR)
 cv2.imshow('resize', imgResize)
+cv2.imshow('resize2', imgResize2)
 key = cv2.waitKey(0)
 cv2.destroyWindow('lenna')
