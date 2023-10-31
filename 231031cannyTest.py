0746ae36a9de54893361f52afd6a78e0dddb16e8
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-31 14:36:06 +0900

    그레이스케일 이미지 불러오기
    cv2.imread(경로, cv2.IMREAD_GRAYSCALE)

--- 231031cannyTest.py
+++ 231031cannyTest.py
@@ -3,7 +3,12 @@ import sys
 
 imgPath = 'data/cannyTest.png'
 imgBGR = cv2.imread(imgPath)
+imgGray = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE)
 
 if imgBGR is None:
     print('Load is Fail!')
     sys.exit()
+
+cv2.imshow('org', imgGray)
+cv2.waitKey()
+cv2.destroyAllWindows()
