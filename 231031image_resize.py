c8dff030702c6c135ab087bc846dd43e5b15f765
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-31 14:29:15 +0900

    크기 조정 1
    원하는 가로세로 사이즈 튜플로 입력

--- 231031image_resize.py
+++ 231031image_resize.py
@@ -9,8 +9,11 @@ if img_BGR is None:
     print('Image Load Fail!')
     sys.exit()
 
+imgResize = cv2.resize(img_BGR, (1280, 720))
+
 print(img_BGR.shape)
 
 cv2.imshow('lenna', img_BGR)
+cv2.imshow('resize', imgResize)
 key = cv2.waitKey(0)
 cv2.destroyWindow('lenna')
