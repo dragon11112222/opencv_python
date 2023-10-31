5b3e1993209f6c18b85583ff6117a7f79dc30619
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-31 14:26:47 +0900

    에러 대비용 코드 작성
    데이터 폴더에 존재하지 않는 파일명을 입력했을 때

--- 231031image_resize.py
+++ 231031image_resize.py
@@ -1,7 +1,12 @@
 import cv2
 import numpy as np
+import sys
 
 imgPath = 'data/picture.jpg'
 img_BGR = cv2.imread(imgPath)
+
+if img_BGR is None:
+    print('Image Load Fail!')
+    sys.exit()
 
 print(img_BGR.shape)
