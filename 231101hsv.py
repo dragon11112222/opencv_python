d22c9aa4f94643b1851c09ee01687873ae0bf0e7
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-11-01 11:19:09 +0900

    색 표시 체계 변경
    bgr에서 hsv로

--- 231101hsv.py
+++ 231101hsv.py
@@ -1,8 +1,11 @@
 import cv2
 import sys
 
-src = cv2.imread('data/trafficLight.png')
+filePath = 'data/trafficLight.png'
+src = cv2.imread(filePath)
 
 if src is None:
     print('Image load failed!')
     sys.exit()
+
+src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
